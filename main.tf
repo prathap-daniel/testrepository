c3e457317f0e8da670a833c851cc18d5de8b9a6a
prathap-daniel <145456234+prathap-daniel@users.noreply.github.com>
2024-04-18 16:39:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,6 @@ resource "aws_instance" "ec2_instance" {
     key_name = "${var.ami_key_pair_name}"
     root_block_device {
         kms_key_id = "arn:aws:kms:us-east-1:925924827205:key/mrk-5a565879268547c8b7c7f484f0efa4eb"
+        encrypted = "true"
     }
 }
