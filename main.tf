6ab7ac3acbe9dba4543caede3e313c72bef4b1ad
prathap-daniel <145456234+prathap-daniel@users.noreply.github.com>
2024-04-18 16:33:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "ec2_instance" {
     subnet_id = "${var.subnet_id}"
     instance_type = "${var.instance_type}"
     key_name = "${var.ami_key_pair_name}"
-    ebs_block_device {
-        kms_key_arn = "arn:aws:kms:us-east-1:925924827205:key/mrk-5a565879268547c8b7c7f484f0efa4eb"
+    root_block_device {
+        kms_key_id = "arn:aws:kms:us-east-1:925924827205:key/mrk-5a565879268547c8b7c7f484f0efa4eb"
     }
 }
